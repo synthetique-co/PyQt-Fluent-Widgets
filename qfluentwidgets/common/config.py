6f31454abc08937e6a8b173918c015ce279c4c64
dave5 <dave@dave5.com>
2024-01-08 13:45:36 +1300

    removed alert altogether

--- qfluentwidgets/common/config.py
+++ qfluentwidgets/common/config.py
@@ -12,9 +12,6 @@ from PySide6.QtGui import QColor
 from .exception_handler import exceptionHandler
 
 
-# ALERT = "\n\033[1;33m📢 Tips:\033[0m QFluentWidgets Pro is now released. Click \033[1;96mhttps://qfluentwidgets.com/pages/pro\033[0m to learn more about it.\n"
-
-
 class Theme(Enum):
     """ Theme enumeration """
 
@@ -404,10 +401,6 @@ class QConfig(QObject):
 
 
 qconfig = QConfig()
-try:
-    print(ALERT)
-except UnicodeEncodeError:
-    print(ALERT.replace("📢", ""))
 
 
 def isDarkTheme():
